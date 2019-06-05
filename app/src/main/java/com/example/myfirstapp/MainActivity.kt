dd7f77d520b9dde59d344ae81f44e5805807bfeb
mduva <techmax@adinet.com.uy>
2019-06-05 12:56:16 -0300

    Primer commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.myfirstapp
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.google.android.gms.tasks.Task
+import androidx.annotation.NonNull
+import androidx.core.content.ContextCompat
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.FusedLocationProviderClient
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.GoogleApiClient
+
+
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+
+    var client : FusedLocationProviderClient? = null
+    var FINE_REQUEST_CODE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        client = LocationServices.getFusedLocationProviderClient(this)
+
+        setupPermissions()
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText2)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE,message)
+        }
+        startActivity(intent)
+        //Do something in response to button
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+
+         if (permission != PackageManager.PERMISSION_GRANTED) {
+             Log.i("PruebaMensaje", "Permission to record denied")
+             makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), FINE_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<String>,grantResults: IntArray) {
+        when (requestCode) {
+            FINE_REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i("test1", "Permission has been denied by user")
+                } else {
+                    Log.i("test2", "Permission has been granted by user")
+                }
+            }
+        }
+
+    }
+}
